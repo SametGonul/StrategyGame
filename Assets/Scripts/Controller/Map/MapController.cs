256266de5056df16e59f0b8bd544df8eacea160d
SametGonul <sametgonul95@gmail.com>
2018-08-05 19:08:54 +0300

    ShortestPath class is created
    Functions related with shortest path were in mapcontroller class, they are moved to new class shortestpath.

--- Assets/Scripts/Controller/Map/MapController.cs
+++ Assets/Scripts/Controller/Map/MapController.cs
@@ -31,18 +31,14 @@ namespace Assets.Scripts.Controller.Map
         private int? _activeCellPositionY = null;
 
         private readonly GridCellModel[,] _gridCellCellArray;
-
-
         private bool _soldierChecker = false;
 
-
         readonly Dictionary<BuildingEventTypes,List<IScrollBuildingModel>> _buildingsList = new Dictionary<BuildingEventTypes, List<IScrollBuildingModel>>();
-
         private readonly List<GameObject> _soldiersList = new List<GameObject>();
         private bool _soldierClickedChecker = false;
 
         private List<GridCellModel> _soldierPath = new List<GridCellModel>();
-        private readonly CellModelForAStar[,] _cellModelAStarArray = new CellModelForAStar[Config.VerticalGridNumber, Config.HorizontalGridNumber];
+        readonly ShortestPath _shortestPath;
 
         private MapController()
         {
@@ -51,6 +47,7 @@ namespace Assets.Scripts.Controller.Map
             _gridCellCellArray = GridController.Instance().GetGridCellArray();
             _buildingsList.Add(BuildingEventTypes.Barrack,new List<IScrollBuildingModel>());
             _buildingsList.Add(BuildingEventTypes.PowerPlant, new List<IScrollBuildingModel>());
+            _shortestPath = new ShortestPath();
         }
 
         public static MapController Instance()
@@ -475,7 +472,8 @@ namespace Assets.Scripts.Controller.Map
                                 if (soldier.GetComponent<SoldierView>().GetSoldierController().GetSoldierHasPath() ==
                                     false)
                                 {
-                                    _soldierPath = GetShortestPathWithAStarAlgorithm(soldier, (int)clickedXIndex, (int)clickedYIndex);
+                                    
+                                    _soldierPath = _shortestPath.GetShortestPathWithAStarAlgorithm(soldier, (int)clickedXIndex, (int)clickedYIndex);
                                     soldier.GetComponent<SoldierView>().GetSoldierController().SetSoldierPath(_soldierPath);
                                     soldier.GetComponent<SoldierView>().GetSoldierController().SetSoldierHasPath(true);
 
@@ -608,173 +606,6 @@ namespace Assets.Scripts.Controller.Map
             soldier.transform.localPosition = _gridCellGameObjectArray[xIndex, yIndex].transform.localPosition;
             _soldiersList.Add(soldier);
         }
-
-
-        // AStar algorithm search function and get shortest path for the soldier with given Target point
-        private List<GridCellModel> GetShortestPathWithAStarAlgorithm(GameObject soldier,int TargetXIndex,int TargetYIndex)
-        {
-            List<CellModelForAStar> openList = new List<CellModelForAStar>();
-            List<CellModelForAStar> closedList = new List<CellModelForAStar>();
-            List<GridCellModel> shortestPath = new List<GridCellModel>();
-            
-
-            CreateCellArrayForAStar();
-            int startGridCellXIndex = (int)soldier.GetComponent<SoldierView>().GetSoldierController().GetSoldierXIndex();
-            int startGridCellYIndex = (int)soldier.GetComponent<SoldierView>().GetSoldierController().GetSoldierYIndex();
-            CellModelForAStar startCell = _cellModelAStarArray[startGridCellXIndex, startGridCellYIndex];
-            startCell.DistanceToEnd = CalculateDistanceValue(startGridCellXIndex, startGridCellYIndex, TargetXIndex, TargetYIndex);
-            startCell.DistanceToStart = 0;
-
-            _cellModelAStarArray[startCell.XIndex, startCell.YIndex].ParentXIndex = -1;
-            _cellModelAStarArray[startCell.XIndex, startCell.YIndex].ParentYIndex = -1;
-
-            openList.Add(startCell);            
-            openList.Remove(startCell);
-
-            CellModelForAStar current = startCell;
-            List<CellModelForAStar> successors = FindSuccessors(current);
-            foreach (var successor in successors)
-            {
-                successor.DistanceToEnd = CalculateDistanceValue(successor.XIndex, successor.YIndex, TargetXIndex, TargetYIndex);
-                successor.DistanceToStart = CalculateDistanceValue(startCell.XIndex, startCell.YIndex, successor.XIndex, successor.YIndex);
-                successor.TotalDistance = successor.DistanceToEnd + successor.DistanceToStart;
-                if (!closedList.Contains(successor))
-                {
-                    _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentXIndex = current.XIndex;
-                    _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentYIndex = current.YIndex;
-                    openList.Add(successor);
-                }
-            }
-            closedList.Add(startCell);
-
-            while (openList.Count > 0)
-            {
-                openList.OrderBy(o => o.DistanceToEnd).ToList();
-                current = openList[0];
-                if (current.XIndex == TargetXIndex && current.YIndex == TargetYIndex)
-                {                   
-                    break;
-                }
-                successors = FindSuccessors(current);
-                foreach (var successor in successors)
-                {
-                   
-                    successor.DistanceToEnd = CalculateDistanceValue(successor.XIndex, successor.YIndex, TargetXIndex, TargetYIndex);
-                    successor.DistanceToStart = CalculateDistanceValue(startCell.XIndex, startCell.YIndex, successor.XIndex,successor.YIndex);
-                    successor.TotalDistance = successor.DistanceToEnd + successor.DistanceToStart;
-                    if (!closedList.Contains(successor) && !openList.Contains(successor))
-                    {
-                        _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentXIndex = current.XIndex;
-                        _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentYIndex = current.YIndex;  
-                        openList.Add(successor);
-                    }
-                }
-                openList.Remove(current);
-                closedList.Add(current);
-            }
-
-            while (_cellModelAStarArray[current.XIndex, current.YIndex].ParentXIndex != -1 &&
-                   _cellModelAStarArray[current.XIndex, current.YIndex].ParentYIndex != -1)
-            {
-                GridCellModel currentGridCellModelPath = new GridCellModel();
-                currentGridCellModelPath.XIndex = current.XIndex;
-                currentGridCellModelPath.YIndex = current.YIndex;
-                shortestPath.Add(currentGridCellModelPath);
-
-                current = _cellModelAStarArray[_cellModelAStarArray[current.XIndex, current.YIndex].ParentXIndex,
-                    _cellModelAStarArray[current.XIndex, current.YIndex].ParentYIndex];
-            }
-
-            shortestPath.Reverse();
-            if (shortestPath[shortestPath.Count - 1].XIndex != TargetXIndex &&
-                shortestPath[shortestPath.Count - 1].YIndex != TargetYIndex)
-            {
-                return null;
-            }
-            return shortestPath;
-        }
-
-        // Create AStar gridcell models for each grid cell
-        private void CreateCellArrayForAStar()
-        {
-            for (int i = 0; i < Config.VerticalGridNumber; i++)
-            {
-                for (int j = 0; j < Config.HorizontalGridNumber; j++)
-                {
-                    CellModelForAStar cellModel = new CellModelForAStar();
-                    _cellModelAStarArray[i, j] = cellModel;
-                    _cellModelAStarArray[i, j].XIndex = i;
-                    _cellModelAStarArray[i, j].YIndex = j;
-                }
-            }
-        }
-
-        // calculates distance between object and endpoint or startingpoint.
-        private double CalculateDistanceValue(int firstXIndex, int firstYIndex, int targetXIndex, int targetYIndex)
-        {
-
-
-            double xDiff;
-            if (targetXIndex >= firstXIndex)
-            {
-                xDiff = (double )(targetXIndex - firstXIndex);
-            }
-            else
-            {
-                xDiff = (double) (firstXIndex - targetXIndex);
-            }
-
-            double yDiff;
-            if (targetYIndex >= firstYIndex)
-            {
-                yDiff = (double) (targetYIndex - firstYIndex);
-                
-            }
-            else
-            {
-                yDiff = (double)(firstYIndex - targetYIndex);
-            }
-
-            return yDiff + xDiff;
-        }
-
-        // Find grid cell successors if they are not building and soldier.
-        private List<CellModelForAStar> FindSuccessors(CellModelForAStar current)
-        {
-            List<CellModelForAStar> successors = new List<CellModelForAStar>();
-            if (current.XIndex + 1 < Config.VerticalGridNumber)
-            {
-                if (_gridCellCellArray[current.XIndex + 1,current.YIndex].GridCellType == GridCellTypes.Empty)
-                {
-                    successors.Add(_cellModelAStarArray[current.XIndex + 1, current.YIndex]);
-                }
-            }
-            if (current.XIndex - 1 >= 0)
-            {
-                
-                if (_gridCellCellArray[current.XIndex - 1, current.YIndex].GridCellType == GridCellTypes.Empty)
-                {
-                    successors.Add(_cellModelAStarArray[current.XIndex -1, current.YIndex]);
-                }
-            }
-            if (current.YIndex + 1 < Config.HorizontalGridNumber)
-            {
-                if (_gridCellCellArray[current.XIndex,current.YIndex + 1].GridCellType == GridCellTypes.Empty)
-                {
-                    successors.Add(_cellModelAStarArray[current.XIndex,current.YIndex + 1]);
-                }
-            }
-            if (current.YIndex - 1 >= 0)
-            {
-                if (_gridCellCellArray[current.XIndex,current.YIndex - 1].GridCellType == GridCellTypes.Empty)
-                {
-                    successors.Add(_cellModelAStarArray[current.XIndex, current.YIndex - 1]);
-                }
-            }
-
-            return successors;
-        }
-
 
         public GameObject[,] GetObjectList()
         {
---
+++ Assets/Scripts/Model/ShortestPath.cs
@@ -0,0 +1,180 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts.Controller.Map;
+using Assets.Scripts.StrategyGame.conf;
+using Assets.Scripts.View.map;
+using UnityEngine;
+
+namespace Assets.Scripts.Model
+{
+    public class ShortestPath {
+
+        private readonly CellModelForAStar[,] _cellModelAStarArray = new CellModelForAStar[Config.VerticalGridNumber, Config.HorizontalGridNumber];
+
+        // AStar algorithm search function and get shortest path for the soldier with given Target point
+        public List<GridCellModel> GetShortestPathWithAStarAlgorithm(GameObject soldier, int TargetXIndex, int TargetYIndex)
+        {
+            List<CellModelForAStar> openList = new List<CellModelForAStar>();
+            List<CellModelForAStar> closedList = new List<CellModelForAStar>();
+            List<GridCellModel> shortestPath = new List<GridCellModel>();
+
+
+            CreateCellArrayForAStar();
+            int startGridCellXIndex = (int)soldier.GetComponent<SoldierView>().GetSoldierController().GetSoldierXIndex();
+            int startGridCellYIndex = (int)soldier.GetComponent<SoldierView>().GetSoldierController().GetSoldierYIndex();
+            CellModelForAStar startCell = _cellModelAStarArray[startGridCellXIndex, startGridCellYIndex];
+            startCell.DistanceToEnd = CalculateDistanceValue(startGridCellXIndex, startGridCellYIndex, TargetXIndex, TargetYIndex);
+            startCell.DistanceToStart = 0;
+
+            _cellModelAStarArray[startCell.XIndex, startCell.YIndex].ParentXIndex = -1;
+            _cellModelAStarArray[startCell.XIndex, startCell.YIndex].ParentYIndex = -1;
+
+            openList.Add(startCell);
+            openList.Remove(startCell);
+
+            CellModelForAStar current = startCell;
+            List<CellModelForAStar> successors = FindSuccessors(current);
+            foreach (var successor in successors)
+            {
+                successor.DistanceToEnd = CalculateDistanceValue(successor.XIndex, successor.YIndex, TargetXIndex, TargetYIndex);
+                successor.DistanceToStart = CalculateDistanceValue(startCell.XIndex, startCell.YIndex, successor.XIndex, successor.YIndex);
+                successor.TotalDistance = successor.DistanceToEnd + successor.DistanceToStart;
+                if (!closedList.Contains(successor))
+                {
+                    _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentXIndex = current.XIndex;
+                    _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentYIndex = current.YIndex;
+                    openList.Add(successor);
+                }
+            }
+            closedList.Add(startCell);
+
+            while (openList.Count > 0)
+            {
+                openList.OrderBy(o => o.DistanceToEnd).ToList();
+                current = openList[0];
+                if (current.XIndex == TargetXIndex && current.YIndex == TargetYIndex)
+                {
+                    break;
+                }
+                successors = FindSuccessors(current);
+                foreach (var successor in successors)
+                {
+
+                    successor.DistanceToEnd = CalculateDistanceValue(successor.XIndex, successor.YIndex, TargetXIndex, TargetYIndex);
+                    successor.DistanceToStart = CalculateDistanceValue(startCell.XIndex, startCell.YIndex, successor.XIndex, successor.YIndex);
+                    successor.TotalDistance = successor.DistanceToEnd + successor.DistanceToStart;
+                    if (!closedList.Contains(successor) && !openList.Contains(successor))
+                    {
+                        _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentXIndex = current.XIndex;
+                        _cellModelAStarArray[successor.XIndex, successor.YIndex].ParentYIndex = current.YIndex;
+                        openList.Add(successor);
+                    }
+                }
+                openList.Remove(current);
+                closedList.Add(current);
+            }
+
+            while (_cellModelAStarArray[current.XIndex, current.YIndex].ParentXIndex != -1 &&
+                   _cellModelAStarArray[current.XIndex, current.YIndex].ParentYIndex != -1)
+            {
+                GridCellModel currentGridCellModelPath = new GridCellModel();
+                currentGridCellModelPath.XIndex = current.XIndex;
+                currentGridCellModelPath.YIndex = current.YIndex;
+                shortestPath.Add(currentGridCellModelPath);
+
+                current = _cellModelAStarArray[_cellModelAStarArray[current.XIndex, current.YIndex].ParentXIndex,
+                    _cellModelAStarArray[current.XIndex, current.YIndex].ParentYIndex];
+            }
+
+            shortestPath.Reverse();
+            if (shortestPath[shortestPath.Count - 1].XIndex != TargetXIndex &&
+                shortestPath[shortestPath.Count - 1].YIndex != TargetYIndex)
+            {
+                return null;
+            }
+            return shortestPath;
+        }
+
+        // Create AStar gridcell models for each grid cell
+        private void CreateCellArrayForAStar()
+        {
+            for (int i = 0; i < Config.VerticalGridNumber; i++)
+            {
+                for (int j = 0; j < Config.HorizontalGridNumber; j++)
+                {
+                    CellModelForAStar cellModel = new CellModelForAStar();
+                    _cellModelAStarArray[i, j] = cellModel;
+                    _cellModelAStarArray[i, j].XIndex = i;
+                    _cellModelAStarArray[i, j].YIndex = j;
+                }
+            }
+        }
+
+        // calculates distance between object and endpoint or startingpoint.
+        private double CalculateDistanceValue(int firstXIndex, int firstYIndex, int targetXIndex, int targetYIndex)
+        {
+
+
+            double xDiff;
+            if (targetXIndex >= firstXIndex)
+            {
+                xDiff = (double)(targetXIndex - firstXIndex);
+            }
+            else
+            {
+                xDiff = (double)(firstXIndex - targetXIndex);
+            }
+
+            double yDiff;
+            if (targetYIndex >= firstYIndex)
+            {
+                yDiff = (double)(targetYIndex - firstYIndex);
+
+            }
+            else
+            {
+                yDiff = (double)(firstYIndex - targetYIndex);
+            }
+
+            return yDiff + xDiff;
+        }
+
+        // Find grid cell successors if they are not building and soldier.
+        private List<CellModelForAStar> FindSuccessors(CellModelForAStar current)
+        {
+            List<CellModelForAStar> successors = new List<CellModelForAStar>();
+            if (current.XIndex + 1 < Config.VerticalGridNumber)
+            {
+                if (MapController.Instance().GetGridCellArray()[current.XIndex + 1, current.YIndex].GridCellType == GridCellTypes.Empty)
+                {
+                    successors.Add(_cellModelAStarArray[current.XIndex + 1, current.YIndex]);
+                }
+            }
+            if (current.XIndex - 1 >= 0)
+            {
+
+                if (MapController.Instance().GetGridCellArray()[current.XIndex - 1, current.YIndex].GridCellType == GridCellTypes.Empty)
+                {
+                    successors.Add(_cellModelAStarArray[current.XIndex - 1, current.YIndex]);
+                }
+            }
+            if (current.YIndex + 1 < Config.HorizontalGridNumber)
+            {
+                if (MapController.Instance().GetGridCellArray()[current.XIndex, current.YIndex + 1].GridCellType == GridCellTypes.Empty)
+                {
+                    successors.Add(_cellModelAStarArray[current.XIndex, current.YIndex + 1]);
+                }
+            }
+            if (current.YIndex - 1 >= 0)
+            {
+                if (MapController.Instance().GetGridCellArray()[current.XIndex, current.YIndex - 1].GridCellType == GridCellTypes.Empty)
+                {
+                    successors.Add(_cellModelAStarArray[current.XIndex, current.YIndex - 1]);
+                }
+            }
+
+            return successors;
+        }
+
+    }
+}
